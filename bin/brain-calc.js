ef44c09e84b381e26ae4c20cd7e9f72ef2b103ab
dastrakhantseva <daria.astrakhantseva@iss.digital>
2024-10-13 22:43:53 +0300

    final

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -3,16 +3,17 @@
 import {enterAnswer, compare, breakCondition} from '../src/index.js';
 import {name} from './brain-games.js';
 
-
 const brainEven = () => {
+
     console.log('What is the result of the expression?')
     let correctAnswers = 1
     while (correctAnswers <= 3) {
-        let number1 = Math.round(Math.random() * 100)
-        let number2 = Math.round(Math.random() * 100)
+
+        const number1 = Math.round(Math.random() * 100)
+        const number2 = Math.round(Math.random() * 100)
         const actions = ['+', '-', '*']
-        let actionsChosen = actions[Math.floor(Math.random() * 3)]
-        let answer = enterAnswer(number1 + ' ' + actionsChosen + ' ' + number2)
+        const actionsChosen = actions[Math.floor(Math.random() * 3)]
+        const answer = enterAnswer(number1 + ' ' + actionsChosen + ' ' + number2)
 
         let correctAnswer;
         switch (actionsChosen) {
@@ -32,7 +33,9 @@ const brainEven = () => {
         if (breakCondition(correctAnswers, name)) {
             break;
         };
+
     };
+
 };
 
 brainEven();
