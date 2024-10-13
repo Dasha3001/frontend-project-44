6a3896caba9f2ee91ce554e1fc50bf0b4be05136
dastrakhantseva <daria.astrakhantseva@iss.digital>
2024-10-13 22:53:39 +0300

    final

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,41 +1,39 @@
 #!/usr/bin/env node
 
-import {enterAnswer, compare, breakCondition} from '../src/index.js';
-import {name} from './brain-games.js';
+import { enterAnswer, compare, breakCondition } from '../src/index.js';
+import { name } from './brain-games.js';
 
 const brainEven = () => {
+  console.log('What is the result of the expression?');
+  let correctAnswers = 1;
+  while (correctAnswers <= 3) {
+    const number1 = Math.round(Math.random() * 100);
+    const number2 = Math.round(Math.random() * 100);
+    const actions = ['+', '-', '*'];
+    const actionsChosen = actions[Math.floor(Math.random() * 3)];
+    const answer = enterAnswer(`${number1} ${actionsChosen} ${number2}`);
 
-    console.log('What is the result of the expression?');
-    let correctAnswers = 1;
-    while (correctAnswers <= 3) {
-
-        const number1 = Math.round(Math.random() * 100);
-        const number2 = Math.round(Math.random() * 100);
-        const actions = ['+', '-', '*'];
-        const actionsChosen = actions[Math.floor(Math.random() * 3)];
-        const answer = enterAnswer(number1 + ' ' + actionsChosen + ' ' + number2);
-
-        let correctAnswer;
-        switch (actionsChosen) {
-        case '+':
-            correctAnswer = (number1 + number2).toString();
-            break;
-        case '-':
-            correctAnswer = (number1 - number2).toString();
-            break;
-        case '*':
-            correctAnswer = (number1 * number2).toString();
-            break;
-        }
-
-        correctAnswers = compare(correctAnswer, answer, correctAnswers, name);
-
-        if (breakCondition(correctAnswers, name)) {
-            break;
-        }
-
+    let correctAnswer;
+    switch (actionsChosen) {
+      case '+':
+        correctAnswer = (number1 + number2).toString();
+        break;
+      case '-':
+        correctAnswer = (number1 - number2).toString();
+        break;
+      case '*':
+        correctAnswer = (number1 * number2).toString();
+        break;
+      default:
+        break;
     }
 
+    correctAnswers = compare(correctAnswer, answer, correctAnswers, name);
+
+    if (breakCondition(correctAnswers, name)) {
+      break;
+    }
+  }
 };
 
 brainEven();
